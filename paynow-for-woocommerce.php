60a88adce99f4cd93d28e411192935f49add41e5
spaghettiwews <spaghettiwews@outlook.com>
2020-05-13 09:14:02 +0200

    Update paynow-for-woocommerce.php

--- paynow-for-woocommerce.php
+++ paynow-for-woocommerce.php
@@ -86,7 +86,7 @@ function woocommerce_paynow_init() {
 			$meta = get_post_meta( $order_id, '_wc_paynow_payment_meta', true );
 			
 			if ( strtolower( $meta['Status'] ) == ps_cancelled ) {
-				wc_add_notice( __( 'You cancelled your payment on paynow.', 'woocommerce' ), 'error' );
+				wc_add_notice( __( 'You cancelled your payment on Paynow.', 'woocommerce' ), 'error' );
 				// wp_redirect( $order->get_cancel_order_url() );
 				wp_redirect( $order->get_checkout_payment_url() );
 			}

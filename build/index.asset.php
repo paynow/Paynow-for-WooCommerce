074b49f33d337455b5ff68390eacc10972e23230
Eliphas Masuka <eliphas@paynow.co.zw>
2024-05-02 15:25:38 +0200

    update new build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element'), 'version' => '004573973ad407f5ad17');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element'), 'version' => '29664ceeea18387b13d9');

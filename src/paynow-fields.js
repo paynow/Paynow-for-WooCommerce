0d8726b04ffd993b50e8234cd67e0c62c232f12b
Eliphas Masuka <eliphas@paynow.co.zw>
2024-05-02 14:28:42 +0200

    add content component

---
+++ src/paynow-fields.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from '@wordpress/element';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { __experimentalUseCheckoutExtensionData } from '@woocommerce/blocks-checkout';
+
+const PaynowCheckoutFields = () => {
+   
+    return (<div>
+       Paynow Zimbabwe
+    </div>);
+};
+
+export default PaynowCheckoutFields;

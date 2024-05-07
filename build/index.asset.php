d6fd15618d61940dd937dd39da201e09cd62f361
eliphas1204 <masukaeliphas@gmail.com>
2024-05-07 09:41:34 +0000

    Register Paynow front end

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element'), 'version' => '29664ceeea18387b13d9');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '775fb5a3df80750f6c3c');
--- src/index.js
+++ src/index.js
@@ -1,27 +1,61 @@
-/**
- * External dependencies
- */
-
+import { sprintf, __ } from '@wordpress/i18n';
 import { registerPaymentMethod } from '@woocommerce/blocks-registry';
+import { decodeEntities } from '@wordpress/html-entities';
+import { getSetting } from '@woocommerce/settings';
+import { registerCheckoutBlock } from '@woocommerce/blocks-checkout';
+import PaynowCheckoutFields from './paynow-fields';
+const settings = getSetting( 'paynow_data', {} );
 
-import PaynowCheckoutFields  from './paynow-fields'
+const defaultLabel = __(
+	'Paynow',
+	'woo-gutenberg-products-block'
+);
+
+const label = decodeEntities( settings.title ) || defaultLabel;
 /**
- * Internal dependencies
+ * Content component
  */
-import './paynow.scss';
-const options = {
-	name: 'paynow',
-	label: 'Paynow',
-	ariaLabel : 'Paynow',
-	content: < PaynowCheckoutFields />,
-	edit: < PaynowCheckoutFields />,
-	canMakePayment: () => true,
-	paymentMethodId: 'paynow',
+const Content = () => {
+	return;
+};
+/**
+ * Label component
+ *
+ * @param {*} props Props from payment API.
+ */
+const Label = ( props ) => {
+	const { PaymentMethodLabel } = props.components;
+	return <PaymentMethodLabel text={ label } />;
+};
 
+/**
+ * Dummy payment method config object.
+ */
+const Paynow = {
+	name: "paynow",
+	label: <Label />,
+	content: <PaynowCheckoutFields />,
+	edit: <PaynowCheckoutFields />,
+	canMakePayment: () => true,
+	ariaLabel: label,
 	supports: {
-		features: [],
+		features: settings.supports,
 	},
 };
 
-registerPaymentMethod(options);
+registerPaymentMethod( Paynow );
 
+// Aliased import
+
+// Global import
+// const { registerCheckoutBlock } = wc.blocksCheckout;
+
+const options = {
+	metadata: {
+		name: 'namespace/paynow-payment-fields',
+		parent: [ 'woocommerce/checkout-totals-block' ],
+	},
+	component: () => <div>A Function Component</div>,
+};
+
+registerCheckoutBlock( options );
